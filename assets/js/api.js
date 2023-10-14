5fa593c50246d4d095e1f50fbf62dd3d22b7f3db
arrrjhay009 <stamaria.ryanjay09@gmail.com>
2023-10-14 21:59:42 +0800

    first commit

---
+++ assets/js/api.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const api_key = "ca76a8992a20704a4cbffc6bdd1a6f54";
+
+export const fetchData = function (url, callback) {
+  fetch(`${url}&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => {
+      console.error(error);
+      alert("Failed to retrieve data. Please try again later.");
+    });
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
